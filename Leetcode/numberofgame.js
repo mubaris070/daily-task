a17b73c7f675c2b208bbfd45d308a94ef7360956
muhammed mubaris <mubarismubaris707@gmail.com>
2025-11-13 10:24:33 +0530

    dailytask

---
+++ Leetcode/numberofgame.js
@@ -0,0 +1,13 @@
+var numberGame = function(nums) {
+        nums.sort((a,b)=>a-b)
+        let result = []
+    for(i=0;i<nums.length;i+=2){
+       let alice = nums[i]
+       let bob = nums[i+1]
+       result.push(alice,bob)
+    }
+    return  result
+    
+    
+};
+console.log(numberGame([4,4,3,8]));
