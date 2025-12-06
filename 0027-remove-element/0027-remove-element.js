aa8bf63a6875656213f71394323b96d0befe5202
Muhammed Mubaris.k <mubarismubaris707@gmail.com>
2025-12-06 09:57:10 +0530

    Time: 0 ms (100%), Space: 53.3 MB (93.98%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,13 @@
+var removeElement = function(nums, val) {
+    let i=0;
+    for(j=0;j<nums.length;j++){
+        if (nums[j] !== val){
+            nums[i] = nums[j];
+            i++
+        }
+    }
+    return i
+
+};
+
+    
